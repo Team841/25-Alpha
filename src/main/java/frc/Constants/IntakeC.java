15c5c76b1b91c530f69e867c1267d58bd94a3d25
Team 841 Programming <thebiomechs@gmail.com>
2025-01-18 14:31:23 -0800

    Configure intake motors

---
+++ src/main/java/frc/Constants/IntakeC.java
@@ -0,0 +1,16 @@
+package frc.Constants;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+public class IntakeC {
+    public static class IntakeConstants {
+        public static int intakeMotorID = 5;
+        public static final TalonFXConfiguration configs = new TalonFXConfiguration();
+        public static final CurrentLimitsConfigs currentLimits = new CurrentLimitsConfigs()
+        .withStatorCurrentLimit(80)
+        .withStatorCurrentLimitEnable(true)
+        .withSupplyCurrentLimitEnable(true);
+    }
+    
+}
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.Constants.IntakeC;
+import frc.Constants.IntakeC.IntakeConstants;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new Intake. */
+  private final TalonFX intakeMotor = new TalonFX(IntakeC.IntakeConstants.intakeMotorID, "rio");
+ 
+  public Intake() {
+  /* Configure the motor's factory default settings */
+    intakeMotor.getConfigurator().apply(IntakeConstants.configs);
+    /*Apply a current configuration to the motor */
+    intakeMotor.getConfigurator().refresh(IntakeConstants.currentLimits);
+    intakeMotor.getConfigurator().apply(IntakeConstants.currentLimits)
+  }
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber(getName(), 0)
+  }
+}
